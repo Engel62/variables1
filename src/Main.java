e2209b709449d8536e1b17606623cb518a7a4bdc
Engel62 <sr.engel@bk.ru>
2022-10-24 13:58:53 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        int age = 17;
+        if (age >= 18) {
+            System.out.println("Ты можешь водить");
+        }
+        if (age < 18) {
+            System.out.println("ТЫ не можешь водить!");
+        }
+    }
+}
