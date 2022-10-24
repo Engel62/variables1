9d10540faeb2ceb6025caf187b787b62aea66f94
Engel62 <sr.engel@bk.ru>
2022-10-24 16:07:51 +0300

    2 задания выполнены

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,38 @@
-import java.sql.SQLOutput;
 
 public class Main {
     public static void main(String[] args) {
-        int age = 17;
+        int age = 20;
         if (age >= 18) {
             System.out.println("Ты можешь водить");
         }
         if (age < 18) {
             System.out.println("ТЫ не можешь водить!");
         }
+        // Задание 1
+        System.out.println("Задание 1");
+        if (age >= 18){
+            System.out.println("Поздравляем с совершеннолетием!");
+        }
+        if (age < 18){
+            System.out.println("возраст совершеннолетия ещё не наступил, нужно немного подождать.");
+        }
+        /// Задание 2
+        System.out.println("Задание 2");
+        int childAge = 25;
+        if (childAge >=7){
+            System.out.println("Ребенок ходит в школу");
+        }
+        if (childAge >= 18){
+            System.out.println("человек уже закончил школу и может отправляться в университет");
+        }
+        if (childAge >= 24){
+            System.out.println("Человек окончил университет и ему пора искать первую работу");
+        }
+        /// Задача 3
+        System.out.println("Задание 3");
+
+
+
+
     }
 }
